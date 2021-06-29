fba4de931f79680f3916252fbb2c712a0ce96543
Jose Lorenzo Faelman <jlfaelman@gmail.com>
2021-06-29 13:56:48 +0800

    Heroku Update

--- conn.js
+++ conn.js
@@ -5,7 +5,7 @@ pg.defaults.ssl = true; //this is it!!!
 require('dotenv').config()
 const pool = new Pool({
     user: process.env.DB_USER,
-    password:process.env.DB_PASS,
+    password:process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
     host:process.env.DB_HOST,
     port:5432,
